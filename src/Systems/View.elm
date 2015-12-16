bdae554ee36f283eab501d1ea41473ba2e41e208
narkisr <narkisr@gmail.com>
2015-12-16 18:20:44 +0200

    impelmeting view

--- src/Systems/View.elm
+++ src/Systems/View.elm
@@ -1,32 +1,72 @@
 module Systems.View where
 
-import Systems.View.AWS  as AWSView
 import Effects exposing (Effects)
+import Systems.Model.Common exposing (System, Machine)
+import Systems.Model.AWS exposing (emptyAws)
+
+-- Effects
+import Task
+import Http exposing (Error(BadResponse))
+import Systems.Decoders exposing (systemDecoder)
+import Common.Redirect exposing (successHandler)
+
+-- View
+import Common.Components exposing (panelContents)
 import Html exposing (..)
-
-
+import Systems.View.AWS  as AWSView
 
 -- Model 
 type alias Model = 
   {
+    system : System
    }
 
 init : (Model , Effects Action)
 init =
-  (Model, Effects.none)
+  let
+    emptySystem = System "" "" "" (Machine  "" "" "" (Just "") "") Nothing
+  in
+   (Model emptySystem, Effects.none)
 
 -- Update
 type Action = 
-  ViewAWS String
+  ViewSystem String
+    | SetSystem (Result Http.Error System)
+    | NoOp
+
+setSystem : Model -> System -> (Model , Effects Action)
+setSystem model system =
+  Debug.log "here" ({model | system = system}, Effects.none)
 
 update : Action ->  Model-> (Model , Effects Action)
 update action model =
   case action of
-    ViewAWS id -> 
-      (model, Effects.none)
+    ViewSystem id -> 
+      (model, getSystem id )
 
+    SetSystem result -> 
+      successHandler result model (setSystem model) NoOp
+      
+    NoOp -> 
+      (model, Effects.none)
+      
 -- View
 
 view : Signal.Address Action -> Model -> List Html
-view address model =
-  [div  [] [text "viewing system"]]
+view address ({system} as model) =
+  case system.aws of
+    Just aws ->
+     panelContents "AWS system" (div [] (AWSView.summarize (aws, system.machine)))
+    Nothing ->
+      [div  [] [text "not implemented"]]
+  
+
+-- Effects
+
+getSystem : String -> Effects Action
+getSystem id = 
+  Http.get systemDecoder ("/systems/" ++ id)
+    |> Task.toResult
+    |> Task.map SetSystem
+    |> Effects.task
+
