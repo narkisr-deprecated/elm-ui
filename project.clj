51fd954bb3f993dbc5830ddbf5d4d43d0e840f9d
narkisr <narkisr@gmail.com>
2016-01-10 18:01:41 +0200

    initial selenium support

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject elm-testing "0.1.0-SNAPSHOT"
+  :dependencies [
+    [clj-webdriver "0.7.2"]
+    [org.seleniumhq.selenium/selenium-java "2.47.1"]
+    [org.seleniumhq.selenium/selenium-chrome-driver "2.47.1"]
+   ])
---
+++ test/elm/ui/add.clj
@@ -0,0 +1,13 @@
+(ns elm.ui.add
+  (:use clj-webdriver.taxi)
+ )
+
+;; (System/setProperty "webdriver.chrome.driver" "/usr/bin/google-chrome")
+;; (with-driver {:browser :chrome}
+;;   (to "https://github.com")
+;;   (click "a[href*='login']")
+;;
+;;   (input-text "#login_field" "your_username")
+;;   (-> "#password"
+;;     (input-text "your_password")
+;;     submit))
