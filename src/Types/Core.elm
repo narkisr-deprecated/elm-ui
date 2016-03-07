9a2d656cee075ae746739fa33c172672c3931de5
narkisr <narkisr@gmail.com>
2016-03-07 15:25:43 +0200

    seperating navigation and routing

--- src/Types/Core.elm
+++ src/Types/Core.elm
@@ -6,6 +6,7 @@ import Types.Add as TypesAdd
 import Types.View as TypesView
 import Nav.Side exposing (Section(Stats, Launch, Add, List, View), Active(Types))
 import Effects exposing (Effects, Never, batch, map)
+import Http exposing (Error(BadResponse))
 import Table as Table
 
 
@@ -36,22 +37,41 @@ type Action =
     | Adding TypesAdd.Action
     | Viewing TypesView.Action
 
-update : Action ->  Model-> (Model , Effects Action)
-update action ({list, add, view} as model) =
+navigate : Action -> (Model , Effects Action) -> (Model , Effects Action)
+navigate action ((({list, add, view} as model), effects) as result) =
+    case action of 
+      Listing listing -> 
+        case listing of 
+          TypesList.LoadPage (Table.View id) ->
+            let 
+              (newSystems, effects) = TypesView.update (TypesView.ViewType id) view
+            in
+              ({model | view = view, navChange = Just (Types, View)}, Effects.map Viewing effects)        
+
+          _ -> 
+            (model, effects)
+
+      Adding adding -> 
+        case adding of 
+          TypesAdd.Saved (Result.Ok _) -> 
+             ({model | navChange = Just (Types, List)}, effects)
+
+          _ -> 
+            (model, effects)
+
+      Viewing viewing -> 
+        (model, effects)
+
+
+
+route : Action ->  Model -> (Model , Effects Action)
+route action ({list, add, view} as model) =
   case action of
     Listing action -> 
-      case action of 
-        TypesList.LoadPage (Table.View id) ->
-          let 
-            (newSystems, effects) = TypesView.update (TypesView.ViewType id) view
-          in
-           ({model | view = view, navChange = Just (Types, View)}, Effects.map Viewing effects)        
-
-        _ -> 
-          let 
-            (newTypes, effect ) = TypesList.update action list
-          in
-            ({ model | list = newTypes }, Effects.map Listing effect)
+      let 
+        (newTypes, effect ) = TypesList.update action list
+      in
+        ({ model | list = newTypes }, Effects.map Listing effect)
 
     Adding action -> 
       let 
@@ -65,6 +85,10 @@ update action ({list, add, view} as model) =
       in
         ({ model | view = newTypes }, Effects.map Viewing effect)
 
+
+update : Action ->  Model-> (Model , Effects Action)
+update action ({list, add, view} as model) =
+  navigate action (route action model)
 
 view : Signal.Address Action -> Model -> Section -> List Html
 view address ({list, add, view} as model) section =
