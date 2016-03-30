081d721341198aa6bec7c56d4488da526f63eb0b
narkisr <narkisr@gmail.com>
2016-03-30 17:59:20 +0300

    enabling add

--- src/Users/Core.elm
+++ src/Users/Core.elm
@@ -4,6 +4,7 @@ import Effects exposing (Effects)
 import Html exposing (..)
 import Nav.Common exposing (Section(Add, List, View, Edit, Delete), Active(Users))
 import Users.List as List exposing (Action)
+import Users.Add as Add exposing (Action)
 import Common.Utils exposing (none)
 
 
@@ -11,6 +12,7 @@ import Common.Utils exposing (none)
 type alias Model = 
   {
    list : List.Model 
+  , add : Add.Model 
   , navChange : Maybe (Active, Section)
   }
  
@@ -18,13 +20,19 @@ init : (Model , Effects Action)
 init =
   let 
     (list, listActions) = List.init
+    (add, addActions) = Add.init
+    effects = [
+        Effects.map Listing listActions
+     ,  Effects.map Adding addActions
+     ]
   in 
-    (Model list Nothing, Effects.map Listing listActions)
+    (Model list add Nothing, Effects.batch effects)
 
 -- Update 
 
 type Action = 
   Listing List.Action
+    | Adding Add.Action
     | MenuClick (String, String)
     | NoOp
 
@@ -47,13 +55,20 @@ navigate action ((({list} as model), effects) as result) =
      none model
 
 route : Action ->  Model -> (Model , Effects Action)
-route action ({list} as model) =
+route action ({list, add} as model) =
   case action of 
     Listing listing -> 
       let
         (newList, effects) =  List.update listing list 
       in
         ({model | list = newList }, Effects.map Listing effects)
+
+    Adding adding -> 
+      let
+        (newAdd, effects) =  Add.update adding add
+      in
+        ({model | add = newAdd }, Effects.map Adding effects)
+
 
     _ -> 
       none model
@@ -66,10 +81,13 @@ update action model =
 -- View
 
 view : Signal.Address Action -> Model -> Section -> List Html
-view address ({list} as model) section =
+view address ({list, add} as model) section =
   case section of 
      List -> 
        List.view (Signal.forwardTo address Listing) list
+
+     Add -> 
+       Add.view (Signal.forwardTo address Adding) add
 
      _ -> 
        [div [] [text "not implemented"]]
